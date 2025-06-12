56363d95ddefc38caca7f6a6f0bbb9fe3a071804
Nikhilsethi7668 <132539777+Nikhilsethi7668@users.noreply.github.com>
2025-06-12 23:53:55 +0530

    post

--- backend/Routes/trips.js
+++ backend/Routes/trips.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const { findTrips } = require("../Controllers/tripController");
 const router = express.Router();
-router.get("/", findTrips);
+router.post("/", findTrips);
 module.exports = router;
